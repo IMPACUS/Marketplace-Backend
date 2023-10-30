f4ed15dd376396f947c2537e20816bfd4fa5ad55
chea-young <lchy0413@gmail.com>
2023-10-30 21:39:22 +0900

    [Feature] add User, UserHistory entity
    Add BaseTimeEntity. Create User, UserHistory entity.
    [Issue No] -

---
+++ src/main/java/com/impacus/maketplace/common/enumType/UserStatus.java
@@ -0,0 +1,14 @@
+package com.impacus.maketplace.common.enumType;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserStatus {
+    ACTIVE(1, "활성화 상태"),
+    BLOCKED (2, "차단된 상태");
+
+    private final int code;
+    private final String value;
+}
