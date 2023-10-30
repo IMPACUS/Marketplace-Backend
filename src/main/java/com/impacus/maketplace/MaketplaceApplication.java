7b99bd963584ff2fec52673b762dcd1e155de199
chea-young <lchy0413@gmail.com>
2023-10-30 20:40:30 +0900

    [Init] create spring boot project
    Add .gitignore about intellij, springboot and application.yml. Create spring boot initial project file
    [Issue No] -

---
+++ src/main/java/com/impacus/maketplace/MaketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.impacus.maketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/impacus/maketplace/MaketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impacus.maketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
