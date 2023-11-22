733dcf32f32b26fa764209c59010af8565f1565e
chea-young <lchy0413@gmail.com>
2023-11-22 22:53:06 +0900

    Feat: add Redis settings
    Add Redis settings
    [Issue No] -

--- src/main/java/com/impacus/maketplace/MaketplaceApplication.java
+++ src/main/java/com/impacus/maketplace/MaketplaceApplication.java
@@ -2,12 +2,16 @@ package com.impacus.maketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.impacus.maketplace.repository")
+@EnableRedisRepositories(basePackages = "com.impacus.maketplace.redis.repository")
 public class MaketplaceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MaketplaceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MaketplaceApplication.class, args);
+    }
 
 }
