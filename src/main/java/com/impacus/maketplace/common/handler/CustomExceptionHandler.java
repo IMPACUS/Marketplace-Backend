5486ccbbd580bfe84cd0c9ae2cb58e3daadc68be
chea-young <lchy0413@gmail.com>
2023-12-13 22:36:03 +0900

    Feature: add log.warn in ExceptionHandler
    Add log.warn in ExceptionHandler
    [Issue No] -

--- src/main/java/com/impacus/maketplace/common/handler/CustomExceptionHandler.java
+++ src/main/java/com/impacus/maketplace/common/handler/CustomExceptionHandler.java
@@ -1,16 +1,25 @@
 package com.impacus.maketplace.common.handler;
 
+import com.impacus.maketplace.common.enumType.error.ErrorType;
 import com.impacus.maketplace.common.exception.CustomException;
 import com.impacus.maketplace.dto.error.response.ErrorDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ErrorDTO> handleCustom400Exception(CustomException ex) {
+        ErrorType errorType = ex.getErrorType();
+        log.warn(
+            String.format("http-status={%d} code={%s} msg={%s} detail={%s}", ex.getStatus().value(),
+                errorType.getCode(), errorType.getMsg(), ex.getDetail())
+        );
+
         return ErrorDTO.toResponseEntity(ex);
     }
 }
