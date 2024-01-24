8d51306bab8ab01c9a73fc686eac152ec89fc392
chea-young <lchy0413@gmail.com>
2024-01-24 19:19:29 +0900

    [Feature] Add adapter to convert LocalDate, LocalDateTime variables
    [Issue No]

--- src/main/java/com/impacus/maketplace/common/utils/ObjectCopyHelper.java
+++ src/main/java/com/impacus/maketplace/common/utils/ObjectCopyHelper.java
@@ -1,11 +1,20 @@
 package com.impacus.maketplace.common.utils;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.impacus.maketplace.common.adapter.LocalDateTimeTypeAdapter;
+import com.impacus.maketplace.common.adapter.LocalDateTypeAdapter;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Component
 public class ObjectCopyHelper {
-    private Gson gson = new Gson();
+    private Gson gson = new GsonBuilder()
+            .registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter())
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter())
+            .create();
 
     public <T> T copyObject(Object sourceObject, Class<T> targetType) {
         if (sourceObject == null) {
