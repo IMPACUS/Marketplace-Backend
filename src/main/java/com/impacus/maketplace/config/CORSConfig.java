f8b3eccd61ab94b8f65e0d5425cf6f9fc3ea4e87
chea-young <lchy0413@gmail.com>
2024-02-22 00:23:27 +0900

    [Feature] add CORS setting
    [Issue No]

--- src/main/java/com/impacus/maketplace/config/CORSConfig.java
+++ src/main/java/com/impacus/maketplace/config/CORSConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -16,8 +15,7 @@ public class CORSConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        //config.setAllowedOrigins(Collections.singletonList("http://localhost:3000, https://back-dev.implace.kr"));
-        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000, https://back-dev.implace.kr"));
         config.setAllowedMethods(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
 
