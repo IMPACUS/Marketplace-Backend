5d7cdf2d1145df92480401f888165fb6a1f4a0c2
Sangmin <sm9171@nate.com>
2024-03-09 01:11:33 +0900

    [Test] Refactor AddToCartTest

--- src/test/java/com/impacus/maketplace/acceptance/OrderAcceptanceTest.java
+++ src/test/java/com/impacus/maketplace/acceptance/OrderAcceptanceTest.java
@@ -1,43 +1,23 @@
 package com.impacus.maketplace.acceptance;
 
-import com.impacus.maketplace.dto.product.request.ProductDetailInfoRequest;
-import com.impacus.maketplace.dto.product.request.ProductOptionRequest;
 import com.impacus.maketplace.dto.shoppingBasket.request.ShoppingBasketRequest;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import static com.impacus.maketplace.acceptance.ProductSteps.*;
+import static com.impacus.maketplace.acceptance.ProductSteps.shoppingBasketRequest;
+import static com.impacus.maketplace.acceptance.ProductSteps.장바구니_추가_요청;
 
 @DisplayName("주문 관련 기능")
 public class OrderAcceptanceTest extends AcceptanceTest {
-
+	private Long 상품옵션 = 1L;
 	@BeforeEach
 	public void setUp() {
 		super.setUp();
 
-		ProductDetailInfoRequest productDetailInfoRequest = createProductDetailInfoRequest("키친타올", "종이", "", "", "별도표기", "", "충남 아산", "키친산업", "", "", "", "");
-
-		List<ProductOptionRequest> productOptionRequests = new ArrayList<>();
-		ProductOptionRequest productOptionRequest1 = new ProductOptionRequest(1L, "", "S", 100L);
-		ProductOptionRequest productOptionRequest2 = new ProductOptionRequest(2L, "", "M", 100L);
-		ProductOptionRequest productOptionRequest3 = new ProductOptionRequest(3L, "", "L", 100L);
-		productOptionRequests.add(productOptionRequest1);
-		productOptionRequests.add(productOptionRequest2);
-		productOptionRequests.add(productOptionRequest3);
-
-		//todo: 상품 등록 요청시에 multipart 오류
-		상품_등록_요청(관리자, 1L, "키친타올", "효과적으로 주방을 청소하는 키친타올", "GENERAL_DELIVERY", "TSHRT", 2500, 2500, 15000, 1500, 750, 230, "SALES_PROGRESS", productDetailInfoRequest, productOptionRequests);
-
-		ShoppingBasketRequest shoppingBasketRequest = shoppingBasketRequest(productOptionRequest1.getProductOptionId(), 80L);
+		ShoppingBasketRequest shoppingBasketRequest = shoppingBasketRequest(상품옵션, 80L);
 
 		ExtractableResponse<Response> response = 장바구니_추가_요청(관리자, shoppingBasketRequest);
 	}
--- src/test/java/com/impacus/maketplace/acceptance/ProductSteps.java
+++ src/test/java/com/impacus/maketplace/acceptance/ProductSteps.java
@@ -1,7 +1,5 @@
 package com.impacus.maketplace.acceptance;
 
-import com.impacus.maketplace.dto.product.request.ProductDetailInfoRequest;
-import com.impacus.maketplace.dto.product.request.ProductOptionRequest;
 import com.impacus.maketplace.dto.shoppingBasket.request.ShoppingBasketRequest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
@@ -9,67 +7,21 @@ import io.restassured.response.Response;
 import org.springframework.http.MediaType;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class ProductSteps {
-	public static ExtractableResponse<Response> 상품_등록_요청(String accessToken, Long brandId, String name, String description, String deliveryType, String categoryType, Integer deliveryFee, Integer refundFee, Integer marketPrice, Integer appSalesPrice, Integer discountPrice, Integer weight, String productStatus, ProductDetailInfoRequest productDetailInfoRequest, List<ProductOptionRequest> productOptionRequests) {
-
-		Map<String, Object> params = new HashMap<>();
-
-		params.put("brandId", brandId);
-		params.put("name", name);
-		params.put("description", description);
-		params.put("deliveryType", deliveryType);
-		params.put("categoryType", categoryType);
-		params.put("deliveryFee", deliveryFee);
-		params.put("refundFee", refundFee);
-		params.put("marketPrice", marketPrice);
-		params.put("appSalesPrice", appSalesPrice);
-		params.put("discountPrice", discountPrice);
-		params.put("weight", weight);
-		params.put("productStatus", productStatus);
-		params.put("productDetailInfoRequest", productDetailInfoRequest);
-		params.put("productOptionRequests", productOptionRequests);
-
-		return RestAssured
-				.given().log().all()
-				.auth().oauth2(accessToken)
-				.body(params)
-				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.when().post("/api/v1/product/seller")
-				.then().log().all().extract();
-	}
-
 	public static ExtractableResponse<Response> 장바구니_추가_요청(String accessToken, ShoppingBasketRequest shoppingBasketRequest) {
 		Map<String, Object> params = new HashMap<>();
-		params.put("shoppingBasketRequest", shoppingBasketRequest);
+		params.put("productOptionId", shoppingBasketRequest.getProductOptionId());
+		params.put("quantity", shoppingBasketRequest.getQuantity());
 
 		return RestAssured
 				.given().log().all()
 				.auth().oauth2(accessToken)
 				.body(params)
 				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.when().post("/api/v1/product/seller")
+				.when().post("/api/v1/shopping-basket/user")
 				.then().log().all().extract();
-	}
-
-	public static ProductDetailInfoRequest createProductDetailInfoRequest(String productType, String productMaterial, String productColor, String productSize, String dateOfManufacture, String washingPrecautions, String countryOfManufacture, String manufacturer, String importer, String qualityAssuranceStandards, String asManager, String contactNumber) {
-		ProductDetailInfoRequest productDetailInfoRequest = new ProductDetailInfoRequest();
-		productDetailInfoRequest.setProductType(productType);
-		productDetailInfoRequest.setProductMaterial(productMaterial);
-		productDetailInfoRequest.setProductColor(productColor);
-		productDetailInfoRequest.setProductSize(productSize);
-		productDetailInfoRequest.setDateOfManufacture(dateOfManufacture);
-		productDetailInfoRequest.setWashingPrecautions(washingPrecautions);
-		productDetailInfoRequest.setCountryOfManufacture(countryOfManufacture);
-		productDetailInfoRequest.setManufacturer(manufacturer);
-		productDetailInfoRequest.setImporter(importer);
-		productDetailInfoRequest.setQualityAssuranceStandards(qualityAssuranceStandards);
-		productDetailInfoRequest.setAsManager(asManager);
-		productDetailInfoRequest.setContactNumber(contactNumber);
-
-		return productDetailInfoRequest;
 	}
 
 	public static ShoppingBasketRequest shoppingBasketRequest(Long productOptionId, Long quantity) {
