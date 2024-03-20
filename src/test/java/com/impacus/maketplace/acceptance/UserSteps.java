903dc071aa0a9b0f8795513fad4fe8c3cb69755b
chea-young <lchy0413@gmail.com>
2024-03-21 00:10:55 +0900

    [Feature] Change get to post in UserSteps Reflect the changed api method
    [Issue No]

--- src/test/java/com/impacus/maketplace/acceptance/UserSteps.java
+++ src/test/java/com/impacus/maketplace/acceptance/UserSteps.java
@@ -11,16 +11,16 @@ import java.util.Map;
 
 public class UserSteps {
 
-	public static ExtractableResponse<Response> 베어러_인증_로그인_요청(String email, String password) {
-		Map<String, String> params = new HashMap<>();
-		params.put("email", email);
-		params.put("password", password);
+    public static ExtractableResponse<Response> 베어러_인증_로그인_요청(String email, String password) {
+        Map<String, String> params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
 
-		return RestAssured.given().log().all()
-				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.body(params)
-				.when().get("/api/v1/auth/login")
-				.then().log().all()
-				.statusCode(HttpStatus.OK.value()).extract();
-	}
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when().post("/api/v1/auth/login")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value()).extract();
+    }
 }
