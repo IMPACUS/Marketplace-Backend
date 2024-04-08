85c713df5f9a40ded35d28b28559ec6487cc818a
chea-young <lchy0413@gmail.com>
2024-04-08 20:45:49 +0900

    [Feature] Add thumbnailUrl in /api/v1/category
    [Issue No]

--- src/main/java/com/impacus/maketplace/dto/category/response/SubCategoryDetailDTO.java
+++ src/main/java/com/impacus/maketplace/dto/category/response/SubCategoryDetailDTO.java
@@ -9,11 +9,14 @@ import lombok.NoArgsConstructor;
 public class SubCategoryDetailDTO {
     private Long subCategoryId;
     private String subCategoryName;
+    private String thumbnailUrl;
 
     @QueryProjection
     public SubCategoryDetailDTO(Long subCategoryId,
-                                String subCategoryName) {
+                                String subCategoryName,
+                                String thumbnailUrl) {
         this.subCategoryId = subCategoryId;
         this.subCategoryName = subCategoryName;
+        this.thumbnailUrl = thumbnailUrl;
     }
 }
