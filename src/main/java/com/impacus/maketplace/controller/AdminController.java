eaa866211f03662f7649f540078ba6f5f2540d5c
BerkleyLim <berkleylim16@gmail.com>
2024-04-15 23:49:06 +0900

    [Comment] Design to API on Admin in description
    - Controller 에서 API 설계 갯수 측정하기
    - 이것은 코멘트만 단 것이며 개발 할 때마다 코드 정리 예정
    
    Marketplace-Backend #63

--- src/main/java/com/impacus/maketplace/controller/AdminController.java
+++ src/main/java/com/impacus/maketplace/controller/AdminController.java
@@ -16,9 +16,35 @@ public class AdminController {
 
 
     /**
-     * 설계도 - 추후 API 설계 이후 정리 예정
+     * 설계도 - 추후 API 개발 하면서 정리 예정
      *
-     * 1) /api/v1/admin/list
+     * 1) /api/v1/admin/list : Get 방식 할지 Post 로 통일 할지 고민중
+     *  - 사용 목적 : 어드민 계정 목록 표시
+     *  - Admin Name / Email / Account type / 전화번호 / 최근 활동 / 활동 내역 / 비밀번호 /
+     *
+     *
+     * 2) /api/v1/admin/create : Post
+     *  - 사용 목적 : 어드민 등록
+     *
+     *
+     * 3) /api/v1/admin/login/log/list : Get 할지 Post로 통일하여 할지 고민 중
+     *  - 사용 목적 : 로그인 내역
+     *
+     *
+     * 4) /api/v1/admin/activity/log : Get 할지 Post 할지 고민중
+     *  - 사용 목적 : 활동 내역
+     *
+     *
+     * 5) /api/v1/admin/update/type : Post
+     *  - 사용 목적 : 계정 유형 변경
+     *
+     *
+     * 6) /api/v1/admin/create/activity/log : Post
+     *   - 활동 내역 로그 추가
+     *
+     *
+     * 7) /api/v1/admin/create/login/log : Post
+     *   - 로그인 로그 추가
      *
      */
 
