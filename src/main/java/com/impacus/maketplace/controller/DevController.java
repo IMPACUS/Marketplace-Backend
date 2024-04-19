ae30bd8afbb35de2498be764bb2ccf20374afcd7
chea-young <lchy0413@gmail.com>
2024-04-19 16:15:10 +0900

    [Feature] Add ValidEnum test controller
    [Issue No]

--- src/main/java/com/impacus/maketplace/controller/DevController.java
+++ src/main/java/com/impacus/maketplace/controller/DevController.java
@@ -1,5 +1,8 @@
 package com.impacus.maketplace.controller;
 
+import com.impacus.maketplace.common.utils.ApiResponseEntity;
+import com.impacus.maketplace.dto.dev.request.TestValidEnumRequest;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,5 +13,13 @@ public class DevController {
     public String oauth(Model model) {
         model.addAttribute("appleUrl", "https://back-dev.implace.kr/oauth2/authorization/apple");
         return "apple-login";
+    }
+
+    @RequestMapping("/test-valid-enum")
+    public ApiResponseEntity<Object> testValidEnum(@Valid TestValidEnumRequest validEnumDTO) {
+        return ApiResponseEntity
+                .builder()
+                .data(validEnumDTO)
+                .build();
     }
 }
