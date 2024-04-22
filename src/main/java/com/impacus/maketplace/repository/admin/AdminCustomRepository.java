40b4211957332c74ff2cbb57af49cede6e876b37
BerkleyLim <berkleylim16@gmail.com>
2024-04-22 22:46:56 +0900

    [Comment] describe to name of findAdminAll()
    - 리파지토리 인터페이스 주석 추가
    
    Marketplace-Backend #63

--- src/main/java/com/impacus/maketplace/repository/admin/AdminCustomRepository.java
+++ src/main/java/com/impacus/maketplace/repository/admin/AdminCustomRepository.java
@@ -5,5 +5,6 @@ import com.impacus.maketplace.dto.admin.AdminUserListDto;
 import java.util.List;
 
 public interface AdminCustomRepository {
+    // 관리자 리스트 출력
     List<AdminUserListDto> findAdminAll();
 }
