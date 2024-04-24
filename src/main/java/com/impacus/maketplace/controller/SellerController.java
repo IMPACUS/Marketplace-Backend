425baa06fb30dfa4c22f7dea0aeb9316b7df0edb
cheayoung.lee <cheayoung.lee@lge.com>
2024-04-24 11:25:05 +0900

    [Feature] add '/api/v1/seller/auth/login
    [Issue no]

--- src/main/java/com/impacus/maketplace/controller/SellerController.java
+++ src/main/java/com/impacus/maketplace/controller/SellerController.java
@@ -1,15 +1,32 @@
 package com.impacus.maketplace.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.impacus.maketplace.common.utils.ApiResponseEntity;
+import com.impacus.maketplace.dto.user.request.LoginRequest;
+import com.impacus.maketplace.dto.user.response.UserDTO;
+import com.impacus.maketplace.service.UserService;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/v1/seller")
 public class SellerController {
+    private final UserService userService;
 
+    @PostMapping("auth/login")
+    public ApiResponseEntity<UserDTO> login(@Valid @RequestBody LoginRequest loginRequest) {
+        UserDTO userDTO = userService.loginSeller(loginRequest);
+
+        return ApiResponseEntity.<UserDTO>builder()
+                .data(userDTO)
+                .build();
+    }
 }
