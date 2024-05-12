5dda24511f2ff5b5f5215fd8119d1934eb9aa1a0
chea-young <lchy0413@gmail.com>
2024-05-13 00:31:57 +0900

    [Refactor] delete set method
    [Issue No] #78

--- src/main/java/com/impacus/maketplace/entity/seller/Seller.java
+++ src/main/java/com/impacus/maketplace/entity/seller/Seller.java
@@ -65,12 +65,4 @@ public class Seller extends BaseEntity {
         this.isDeleted = false;
         this.chargePercent = 0;
     }
-
-    public void setEntryStatus(EntryStatus entryStatus) {
-        this.entryStatus = entryStatus;
-    }
-
-    public void setChargePercent(int chargePercent) {
-        this.chargePercent = chargePercent;
-    }
 }
