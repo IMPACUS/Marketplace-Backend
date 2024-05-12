5a665840b4fd257614ef8549fe54ca826fa6462c
chea-young <lchy0413@gmail.com>
2024-05-12 22:32:07 +0900

    [Refactor] Error occurrence test code annotation processing
    [Issue No] #78

--- src/test/java/com/impacus/maketplace/acceptance/OrderSteps.java
+++ src/test/java/com/impacus/maketplace/acceptance/OrderSteps.java
@@ -1,8 +1,5 @@
 package com.impacus.maketplace.acceptance;
 
-import com.impacus.maketplace.common.enumType.OrderStatus;
-import com.impacus.maketplace.common.enumType.PaymentMethod;
-import com.impacus.maketplace.dto.order.request.CreateOrderRequest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
@@ -12,20 +9,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class OrderSteps {
-    public static ExtractableResponse<Response> 주문_추가_요청(String accessToken, CreateOrderRequest request) {
-        Map<String, Object> params = new HashMap<>();
-        params.put("shoppingBasketId", request.getShoppingBasketId());
-        params.put("paymentMethod", request.getPaymentMethod());
-        params.put("orderStatus", request.getOrderStatus());
-
-        return RestAssured
-                .given().log().all()
-                .auth().oauth2(accessToken)
-                .body(params)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/api/v1/order")
-                .then().log().all().extract();
-    }
+//    public static ExtractableResponse<Response> 주문_추가_요청(String accessToken, CreateOrderRequest request) {
+//        Map<String, Object> params = new HashMap<>();
+//        params.put("shoppingBasketId", request.getShoppingBasketId());
+//        params.put("paymentMethod", request.getPaymentMethod());
+//        params.put("orderStatus", request.getOrderStatus());
+//
+//        return RestAssured
+//                .given().log().all()
+//                .auth().oauth2(accessToken)
+//                .body(params)
+//                .contentType(MediaType.APPLICATION_JSON_VALUE)
+//                .when().post("/api/v1/order")
+//                .then().log().all().extract();
+//    }
 
     public static ExtractableResponse<Response> 주문_전체_조회_요청(String accessToken, String startAt, String endAt) {
         Map<String, Object> params = new HashMap<>();
@@ -41,12 +38,12 @@ public class OrderSteps {
                 .then().log().all().extract();
     }
 
-    public static CreateOrderRequest createCreateOrderRequest(Long shoppingBasketId, OrderStatus orderStatus, PaymentMethod paymentMethod) {
-        CreateOrderRequest createOrderRequest = new CreateOrderRequest();
-        createOrderRequest.setShoppingBasketId(shoppingBasketId);
-        createOrderRequest.setOrderStatus(orderStatus);
-        createOrderRequest.setPaymentMethod(paymentMethod);
-
-        return createOrderRequest;
-    }
+//    public static CreateOrderRequest createCreateOrderRequest(Long shoppingBasketId, OrderStatus orderStatus, PaymentMethod paymentMethod) {
+//        CreateOrderRequest createOrderRequest = new CreateOrderRequest();
+//        createOrderRequest.setShoppingBasketId(shoppingBasketId);
+//        createOrderRequest.setOrderStatus(orderStatus);
+//        createOrderRequest.setPaymentMethod(paymentMethod);
+//
+//        return createOrderRequest;
+//    }
 }
