9841a49735d0a62f8e7f7ed00e8c40daafb6dd55
chea-young <lchy0413@gmail.com>
2024-05-18 22:06:07 +0900

    [Feature] Add MyPaymentMethod entity [Issue No] #87

--- src/main/java/com/impacus/maketplace/dto/user/request/SignUpDTO.java
+++ src/main/java/com/impacus/maketplace/dto/user/request/SignUpDTO.java
@@ -1,15 +1,43 @@
 package com.impacus.maketplace.dto.user.request;
 
+import com.impacus.maketplace.common.annotation.ValidBirthDate;
+import com.impacus.maketplace.common.annotation.ValidPhoneNumber;
+import com.impacus.maketplace.dto.address.AddressAddOrUpdateRequest;
+import com.impacus.maketplace.dto.paymentMethod.request.CreatePaymentMethodDTO;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignUpDTO {
-    private String email;
-    private String password;
-    private String name;
 
+    @NotBlank(message = "이메일은 공백이 될 수 없습니다.")
+    @Email(message = "올바른 형식의 이메일을 입력하세요.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 공백이 될 수 없습니다.")
+    private String password;
+
+    @NotBlank
+    @ValidPhoneNumber
+    private String phoneNumber;
+
+    @NotBlank
+    @ValidBirthDate
+    private String birthDate;
+
+    @NotNull
+    private List<AddressAddOrUpdateRequest> addresses;
+
+    @NotNull
+    private List<CreatePaymentMethodDTO> paymentMethods;
+
+    private String name;
 }
