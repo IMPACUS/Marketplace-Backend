a6ea628b5662f121637e4856e5ce1c59e0f91bd3
chea-young <lchy0413@gmail.com>
2024-05-18 11:53:25 +0900

    [Feature] Set liquibase 1. build.gradle에 liquibase 설정 추가 2. db/changelog에 liquibase 설정 추가 3. application.yml의 liquibase 설정 추가
    [Issue No] #89

--- src/test/java/com/impacus/maketplace/utils/DataLoader.java
+++ src/test/java/com/impacus/maketplace/utils/DataLoader.java
@@ -1,11 +1,7 @@
 package com.impacus.maketplace.utils;
 
-import com.impacus.maketplace.common.enumType.DeliveryType;
-import com.impacus.maketplace.common.enumType.DiscountStatus;
 import com.impacus.maketplace.common.enumType.OauthProviderType;
-import com.impacus.maketplace.common.enumType.ProductStatus;
 import com.impacus.maketplace.common.utils.StringUtils;
-import com.impacus.maketplace.entity.product.Product;
 import com.impacus.maketplace.entity.product.ProductOption;
 import com.impacus.maketplace.entity.user.User;
 import com.impacus.maketplace.repository.UserRepository;
@@ -32,7 +28,7 @@ public class DataLoader {
 
     public void loadData() {
         userRepository.save(new User(StringUtils.createStrEmail("admin@email.com", OauthProviderType.NONE), passwordEncoder.encode("password1234!@#$"), "admin"));
-        productRepository.save(new Product(1L, 1L, "아디다스 슈퍼스타", "1", DeliveryType.GENERAL_DELIVERY, 1L, 3000, 3000, 10000, 10000, 8000, 100, false, ProductStatus.SALES_PROGRESS, DiscountStatus.DISCOUNT_PROGRESS));
+        //productRepository.save(new Product(1L, 1L, "아디다스 슈퍼스타", "1", DeliveryType.GENERAL_DELIVERY, 1L, 3000, 3000, 10000, 10000, 8000, 100, false, ProductStatus.SALES_PROGRESS, DiscountStatus.DISCOUNT_PROGRESS));
         productOptionRepository.save(new ProductOption(1L, 1L, "blue", "230", 200L, false));
         productOptionRepository.save(new ProductOption(2L, 1L, "black", "240", 180L, false));
     }
