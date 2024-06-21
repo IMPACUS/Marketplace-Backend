716d92e66c9090e62e8c95c159ea8443f0ef851b
chea-young <lchy0413@gmail.com>
2024-06-21 20:31:47 +0900

    [Feature] Add errorType and delete code, msg
    [Issue No] #105

--- src/main/java/com/impacus/maketplace/common/exception/CustomException.java
+++ src/main/java/com/impacus/maketplace/common/exception/CustomException.java
@@ -9,58 +9,50 @@ import org.springframework.http.HttpStatus;
 public class CustomException extends RuntimeException {
 
     private final HttpStatus status;
-    private final String errorCode;
-    private final String errorMsg;
     private final Object detail;
+    private final ErrorType errorType;
 
     public CustomException(HttpStatus status, ErrorType errorType) {
         this.status = status;
-        this.errorCode = errorType.getCode();
-        this.errorMsg = errorType.getMsg();
         this.detail = "";
+        this.errorType = errorType;
     }
 
     public CustomException(HttpStatus status, ErrorType errorType, Object detail) {
         this.status = status;
-        this.errorCode = errorType.getCode();
-        this.errorMsg = errorType.getMsg();
         this.detail = detail;
+        this.errorType = errorType;
     }
 
     public CustomException(ErrorType errorType) {
         this.status = HttpStatus.BAD_REQUEST;
-        this.errorCode = errorType.getCode();
-        this.errorMsg = errorType.getMsg();
         this.detail = "";
+        this.errorType = errorType;
     }
 
     public CustomException(ErrorType errorType, Object detail) {
         this.status = HttpStatus.BAD_REQUEST;
-        this.errorCode = errorType.getCode();
-        this.errorMsg = errorType.getMsg();
         this.detail = detail;
+        this.errorType = errorType;
     }
 
 
     public CustomException(ErrorType errorType, Throwable cause) {
         this.status = HttpStatus.BAD_REQUEST;
-        this.errorCode = errorType.getCode();
-        this.errorMsg = errorType.getMsg();
         this.detail = cause.getMessage();
+        this.errorType = errorType;
     }
 
     public CustomException(Exception exception) {
         if (exception.getClass() == CustomException.class) {
             CustomException customException = (CustomException) exception;
             this.status = customException.getStatus();
-            this.errorCode = customException.errorCode;
-            this.errorMsg = customException.errorMsg;
             this.detail = customException.getDetail();
+            this.errorType = customException.errorType;
         } else {
             this.status = HttpStatus.BAD_REQUEST;
-            this.errorCode = CommonErrorType.UNKNOWN.getCode();
-            this.errorMsg = "";
             this.detail = exception.getMessage();
+            this.errorType = CommonErrorType.UNKNOWN;
         }
     }
 
--- src/main/java/com/impacus/maketplace/dto/error/response/ErrorDTO.java
+++ src/main/java/com/impacus/maketplace/dto/error/response/ErrorDTO.java
@@ -1,5 +1,6 @@
 package com.impacus.maketplace.dto.error.response;
 
+import com.impacus.maketplace.common.enumType.error.ErrorType;
 import com.impacus.maketplace.common.exception.CustomException;
 import lombok.Builder;
 import org.springframework.http.ResponseEntity;
@@ -7,14 +8,26 @@ import org.springframework.http.ResponseEntity;
 @Builder
 public record ErrorDTO(String code, String msg, Object detail) {
     public static ResponseEntity<Object> toResponseEntity(CustomException ex) {
+        ErrorType errorType = ex.getErrorType();
         Object detail = ex.getDetail();
 
         return ResponseEntity
                 .status(ex.getStatus())
-                .body(ErrorDTO.builder()
-                        .code(ex.getErrorCode())
-                        .msg(ex.getErrorMsg())
-                        .detail(detail)
-                        .build());
+                .body(ErrorDTO.toDTO(errorType, detail));
+    }
+
+    public static ErrorDTO toDTO(ErrorType errorType) {
+        return ErrorDTO.builder()
+                .code(errorType.getCode())
+                .msg(errorType.getMsg())
+                .build();
+    }
+
+    public static ErrorDTO toDTO(ErrorType errorType, Object detail) {
+        return ErrorDTO.builder()
+                .code(errorType.getCode())
+                .msg(errorType.getMsg())
+                .detail(detail)
+                .build();
     }
 }
