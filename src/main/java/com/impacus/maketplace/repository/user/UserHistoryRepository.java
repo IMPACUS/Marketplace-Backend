993c9fd706fc4a56f087e848ac9201cef18993e1
chea-young <lchy0413@gmail.com>
2024-07-01 21:03:07 +0900

    [Feature] Delete unused code - 이전 개발자분이 추가한 코드로 이력관리가 되지 않는 테스트 코드와 보류 상태로 두기로한 Entity 파일 삭제
    [Issue No]

--- src/main/java/com/impacus/maketplace/repository/user/UserHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.impacus.maketplace.repository.user;
-
-import com.impacus.maketplace.entity.user.UserHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserHistoryRepository extends JpaRepository<UserHistory, Long> {
-}
