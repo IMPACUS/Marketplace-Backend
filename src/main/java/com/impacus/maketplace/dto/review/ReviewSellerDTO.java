18cb49059b1456351e9f0b166c971ab90209bdd5
BerkleyLim <berkleylim16@gmail.com>
2024-07-12 22:02:24 +0900

    [Feat] add Review
    - 판매자용 리뷰 리스트 API 제작
    
    Marketplace-Backend #104

---
+++ src/main/java/com/impacus/maketplace/dto/review/ReviewSellerDTO.java
@@ -0,0 +1,38 @@
+package com.impacus.maketplace.dto.review;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ReviewSellerDTO {
+    private Long id; // 리뷰 인덱스 번호
+    private Long orderId; // 주문 인덱스 번호
+    private Long sellerId; // 판매자 인덱스 번호
+    private Long buyerId; // 구매자 인덱스 번호
+    private Integer score; // 점수
+    private String buyerContents; // 구매자 리뷰 내용
+    private Long buyerUploadImgId; // 구매자 업로드 이미지 번호
+    private String sellerComment; // 판매자 답글
+    private ZonedDateTime createAt; // 리뷰 생성일
+    private Boolean isArchive; // 삭제 시 아카이브 여부
+    private ZonedDateTime archiveAt; // 아카이브 시작점 (Spring 스케쥴링 기법으로 자동 삭제 여부 확인)
+
+    private String buyerName; // 주문자 표시 (웹 - 판매자 사이트)
+    private String idName; // 주문자 아이디
+
+    // 아래는 product_detail_info
+    private String productColor;
+    private Long productId;
+    private String productMaterial;
+    private String productSize;
+    private String productType;
+
+    // 아래는 purchase_product
+    private Integer quantity;
+    private Integer totalPrice;
+}
--- src/main/java/com/impacus/maketplace/repository/review/ReviewCustomRepositoryImpl.java
+++ src/main/java/com/impacus/maketplace/repository/review/ReviewCustomRepositoryImpl.java
@@ -2,7 +2,7 @@ package com.impacus.maketplace.repository.review;
 
 import com.impacus.maketplace.dto.review.QReviewBuyerDTO;
 import com.impacus.maketplace.dto.review.ReviewBuyerDTO;
-import com.impacus.maketplace.dto.review.ReviewDTO;
+import com.impacus.maketplace.dto.review.ReviewSellerDTO;
 import com.impacus.maketplace.entity.order.QOrder;
 import com.impacus.maketplace.entity.order.QPurchaseProduct;
 import com.impacus.maketplace.entity.product.QProductDetailInfo;
@@ -10,8 +10,12 @@ import com.impacus.maketplace.entity.review.QReview;
 import com.impacus.maketplace.entity.seller.QSeller;
 import com.impacus.maketplace.entity.user.QUser;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -30,8 +34,8 @@ public class ReviewCustomRepositoryImpl implements ReviewCustomRepository {
 
     /**
      * 구매자 전용 - 해당 상품 구매한 리뷰 보기
+     *
      * @param userId 회원 번호 (로그인)
-     * @param orderId 주문 번호 인덱스
      * @return
      */
     @Override
@@ -66,6 +70,13 @@ public class ReviewCustomRepositoryImpl implements ReviewCustomRepository {
         return reviewBuyerDTO;
     }
 
+    /**
+     * (2) 구매자용 리뷰 상세보기
+     *
+     * @param userId
+     * @param orderId
+     * @return
+     */
     @Override
     public ReviewBuyerDTO displayViewBuyerReviewOne(Long userId, Long orderId) {
         BooleanBuilder builder = new BooleanBuilder();
@@ -86,4 +97,62 @@ public class ReviewCustomRepositoryImpl implements ReviewCustomRepository {
                 .fetchOne();
         return reviewBuyerDTO;
     }
+
+    @Override
+    public Slice<ReviewSellerDTO> displaySellerReviewList(Pageable pageable, Long userId) {
+        List<ReviewSellerDTO> results = queryFactory.select(
+                        Projections.fields(ReviewSellerDTO.class,
+                                review.id,
+                                review.orderId,
+                                review.sellerId,
+                                review.buyerId,
+                                review.score,
+                                review.buyerContents,
+                                review.sellerComment,
+                                review.createAt,
+                                review.buyerContents,
+                                review.isComment,
+                                userEntity.name
+                        )
+                ).from(review).innerJoin(userEntity).on(review.sellerId.eq(userEntity.id))
+                .where(review.sellerId.eq(userId))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1)
+                .orderBy(review.createAt.desc())
+                .fetch();
+        boolean hasNext = results.size() > pageable.getPageSize();
+        if (hasNext) {
+            results.remove(results.size() - 1);
+        }
+        return new SliceImpl<>(results, pageable, hasNext);
+    }
+
+    /**
+     *     private Long id; // 리뷰 인덱스 번호
+     *     private Long orderId; // 주문 인덱스 번호
+     *     private Long sellerId; // 판매자 인덱스 번호
+     *     private Long buyerId; // 구매자 인덱스 번호
+     *     private Integer score; // 점수
+     *     private String buyerContents; // 구매자 리뷰 내용
+     *     private Long buyerUploadImgId; // 구매자 업로드 이미지 번호
+     *     private String sellerComment; // 판매자 답글
+     *     private ZonedDateTime createAt; // 리뷰 생성일
+     *     private Boolean isArchive; // 삭제 시 아카이브 여부
+     *     private ZonedDateTime archiveAt; // 아카이브 시작점 (Spring 스케쥴링 기법으로 자동 삭제 여부 확인)
+     *
+     *     private String buyerName; // 주문자 표시 (웹 - 판매자 사이트)
+     *     private String idName; // 주문자 아이디
+     *
+     *     // 아래는 product_detail_info
+     *     private String productColor;
+     *     private Long productId;
+     *     private String productMaterial;
+     *     private String productSize;
+     *     private String productType;
+     *
+     *     // 아래는 purchase_product
+     *     private Integer quantity;
+     *     private Integer totalPrice;
+     */
+
 }
