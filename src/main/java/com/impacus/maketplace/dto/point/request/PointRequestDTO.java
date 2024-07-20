1405dd3221547acba456246fadd3cf5e02108ce8
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-07-20 15:34:53 +0900

    chore: 이전 쿠폰 작업 로직 삭제

--- src/main/java/com/impacus/maketplace/dto/point/request/PointRequestDTO.java
+++ src/main/java/com/impacus/maketplace/dto/point/request/PointRequestDTO.java
@@ -1,26 +1,26 @@
-package com.impacus.maketplace.dto.point.request;
-
-import com.impacus.maketplace.common.enumType.PointType;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class PointRequestDTO {
-    private Long userId;
-
-    private int savePoint;
-
-    @Builder.Default
-    private boolean isManual = false;
-
-    private String pointCode;
-
-    public PointType getPointTypeEnum() {
-        return PointType.fromCode(this.pointCode);
-    }
-}
+//package com.impacus.maketplace.dto.point.request;
+//
+//import com.impacus.maketplace.common.enumType.PointType;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+//public class PointRequestDTO {
+//    private Long userId;
+//
+//    private int savePoint;
+//
+//    @Builder.Default
+//    private boolean isManual = false;
+//
+//    private String pointCode;
+//
+//    public PointType getPointTypeEnum() {
+//        return PointType.fromCode(this.pointCode);
+//    }
+//}
