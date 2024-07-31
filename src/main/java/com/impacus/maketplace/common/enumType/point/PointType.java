cb68225c1d5deb94edd0704a82ebcd0a52e1c446
chea-young <lchy0413@gmail.com>
2024-08-01 00:10:15 +0900

    [Feature] Add function to pay level point - 레벨 포인트 지급 시 관련 함수 추 [Issue no] #136

--- src/main/java/com/impacus/maketplace/common/enumType/point/PointType.java
+++ src/main/java/com/impacus/maketplace/common/enumType/point/PointType.java
@@ -3,25 +3,19 @@ package com.impacus.maketplace.common.enumType.point;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.Arrays;
-
 @Getter
 @RequiredArgsConstructor
 public enum PointType {
-    JOIN("00","회원가입"),
-    SAVE("10","적립"),
-    USE("20","결제 시 사용"),
-    EXPIRE("30","소멸"),
-    CHECK("40", "출석체크"),
-    DORMANCY("50", "휴면에 의한 포인트 감소"),
-    ADMIN_PROVIDE("60","관리자에 의한 포인트 지급"),
-    ADMIN_RECEIVE("70","관리자에 의한 포인트 감소");
+    JOIN(0, "회원가입"),
+    PURCHASE_PRODUCT(1, "상품 구매"),
+    USE(2, "결제 시 사용"),
+    EXPIRE(3, "소멸"),
+    CHECK(4, "출석체크"),
+    DORMANCY(5, "휴면에 의한 포인트 감소"),
+    ADMIN_PROVIDE(6, "관리자에 의한 포인트 지급"),
+    ADMIN_RECEIVE(7, "관리자에 의한 포인트 감소");
 
-    private final String code;
+
+    private final int code;
     private final String value;
-
-    public static PointType fromCode(String code) {
-        return Arrays.stream(PointType.values()).filter(t -> t.getCode().equals(code)).findFirst()
-                .orElseThrow();
-    }
 }
--- src/main/java/com/impacus/maketplace/repository/point/levelPoint/LevelPointMasterRepository.java
+++ src/main/java/com/impacus/maketplace/repository/point/levelPoint/LevelPointMasterRepository.java
@@ -1,11 +1,35 @@
 package com.impacus.maketplace.repository.point.levelPoint;
 
+import com.impacus.maketplace.common.enumType.user.UserLevel;
+import com.impacus.maketplace.entity.point.levelPoint.LevelPointMaster;
+import jakarta.persistence.LockModeType;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.impacus.maketplace.entity.point.levelPoint.LevelPointMaster;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Repository
 public interface LevelPointMasterRepository extends JpaRepository<LevelPointMaster, Long>{
-    
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT lpm FROM LevelPointMaster lpm where lpm.userId = :userId")
+    Optional<LevelPointMaster> findByUserIdForUpdate(@Param("userId") Long userId);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE LevelPointMaster lpm " +
+            "SET lpm.levelPoint = :levelPoint, lpm.userLevel =:userLevel, lpm.expirationStartAt = :expirationStartAt " +
+            "WHERE lpm.userId = :userId")
+    int updateLevelPointAndExpiredAt(
+            @Param("levelPoint") Long levelPoint,
+            @Param("userLevel") UserLevel userLevel,
+            @Param("expirationStartAt") LocalDateTime expirationStartAt,
+            @Param("userId") Long userId
+    );
 }
