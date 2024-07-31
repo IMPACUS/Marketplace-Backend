953aa9b8277f3fb583622c7281b4257da3141fe6
chea-young <lchy0413@gmail.com>
2024-07-31 18:40:47 +0900

    [Refactor] Separate ProductController for CRUD [Issue no] #133

---
+++ src/main/java/com/impacus/maketplace/controller/product/DeleteProductController.java
@@ -0,0 +1,43 @@
+package com.impacus.maketplace.controller.product;
+
+import com.impacus.maketplace.common.utils.ApiResponseEntity;
+import com.impacus.maketplace.service.product.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import security.CustomUserDetails;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/product")
+public class DeleteProductController {
+    private final ProductService productService;
+
+    /**
+     * 상품 다중 삭제 API
+     *
+     * @param productIdList
+     * @return
+     */
+    @PreAuthorize("hasRole('ROLE_APPROVED_SELLER') " +
+            "or hasRole('ROLE_ADMIN') " +
+            "or hasRole('ROLE_PRINCIPAL_ADMIN')" +
+            "or hasRole('ROLE_OWNER')")
+    @DeleteMapping("")
+    public ApiResponseEntity<Boolean> deleteAllProduct(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @RequestParam(name = "product-id") List<Long> productIdList
+    ) {
+        productService.deleteAllProduct(user.getId(), productIdList);
+        return ApiResponseEntity.simpleResult(HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/impacus/maketplace/controller/product/UpdateProductController.java
@@ -0,0 +1,57 @@
+package com.impacus.maketplace.controller.product;
+
+import com.impacus.maketplace.common.utils.ApiResponseEntity;
+import com.impacus.maketplace.dto.product.request.UpdateProductDTO;
+import com.impacus.maketplace.dto.product.response.ProductDTO;
+import com.impacus.maketplace.service.product.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import security.CustomUserDetails;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/product")
+public class UpdateProductController {
+    private final ProductService productService;
+
+    /**
+     * 등록된 상품을 수정하는 API
+     *
+     * @param productImageList
+     * @param productDescriptionImageList
+     * @param dto
+     * @return
+     */
+    @PreAuthorize("hasRole('ROLE_APPROVED_SELLER') " +
+            "or hasRole('ROLE_ADMIN') " +
+            "or hasRole('ROLE_PRINCIPAL_ADMIN')" +
+            "or hasRole('ROLE_OWNER')")
+    @PutMapping("")
+    public ApiResponseEntity<ProductDTO> updateProduct(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @RequestPart(value = "productImage", required = false) List<MultipartFile> productImageList,
+            @RequestPart(value = "productDescriptionImage", required = false) List<MultipartFile> productDescriptionImageList,
+            @Valid @RequestPart(value = "product") UpdateProductDTO dto) {
+        ProductDTO productDTO = productService.updateProduct(
+                user.getId(),
+                productImageList,
+                dto,
+                productDescriptionImageList
+        );
+        return ApiResponseEntity
+                .<ProductDTO>builder()
+                .data(productDTO)
+                .build();
+    }
+}
