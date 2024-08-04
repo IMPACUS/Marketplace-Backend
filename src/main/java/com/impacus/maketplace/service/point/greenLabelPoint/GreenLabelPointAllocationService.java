3ac7ed7f6e739a20fbb99589bd85200614259883
chea-young <lchy0413@gmail.com>
2024-08-04 21:03:54 +0900

    [Feature] When level points are paid, connect when green label points are paid [Issue no]  #140

--- src/main/java/com/impacus/maketplace/service/point/greenLabelPoint/GreenLabelPointAllocationService.java
+++ src/main/java/com/impacus/maketplace/service/point/greenLabelPoint/GreenLabelPointAllocationService.java
@@ -73,18 +73,40 @@ public class GreenLabelPointAllocationService {
 
     /**
      * 그린 라벨 포인트를 차감하는 함수
+     * - 사용된 포인트 만큼 [그린 라벨 포인트 지급] 데이터들을 사용된 상태로 업데이트하고, 사용된 이력을 새로운 [그린 라벨 포인트 이력] 저장
      *
      * @param userId     그린 라벨 포인트를 차감할 사용자 아이디
      * @param type       포인트 이력 타입 (예: 상품 구매, 상품 환불, 출석 체크 등)
      * @param usedPoints 사용된 포인트
-     * @return 포인트 차감 이력 저장된 greenLabelPointHistory의 id
+     * @param allowsNegativeBalance 포인트가 부족하더라도 차감할지 여부
+     * @return 포인트 차감 이력 저장된 greenLabelPointHistory 의 id
      */
     public Long deductPoints(
             Long userId,
             PointType type,
             Long usedPoints,
-            @Nullable boolean lkjkljlksdfs
+            @Nullable Boolean allowsNegativeBalance
     ) {
+        if (allowsNegativeBalance == null) {
+            allowsNegativeBalance = false;
+        }
+
         return null;
+    }
+
+    /**
+     * 포인트 사용 취소를 하는 함수
+     * - 그린 라벨 포인트 이력에 연관된 [그린 라벨 포인트 지급] 데이터에서 사용되었던 포인트 만큼 원복시키고, 업데이트된 포인트 이력을 새로운 [그린 라벨 포인트 이력]에 저장
+     *
+     * @param userId                   그린 라벨 포인트를 사용 취소할 사용자 아이디
+     * @param type                     포인트 이력 타입
+     * @param greenLabelPointHistoryId 주문이력에서 사용된 포인트 차감 이력 id
+     */
+    public void cancelUsedPoint(
+            Long userId,
+            PointType type,
+            Long greenLabelPointHistoryId
+    ) {
+
     }
 }
