de0650fb1a78a23d6a261ec1ae465047e95cd8f4
chea-young <lchy0413@gmail.com>
2024-08-05 21:49:53 +0900

    [Feature] Add GET /api/v1/green-label-point [Issue no]  #140

--- src/main/java/com/impacus/maketplace/repository/point/greenLabelPoint/querydsl/GreenLabelPointAllocationCustomRepositoryImpl.java
+++ src/main/java/com/impacus/maketplace/repository/point/greenLabelPoint/querydsl/GreenLabelPointAllocationCustomRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.impacus.maketplace.repository.point.greenLabelPoint.querydsl;
 
 import com.impacus.maketplace.common.enumType.point.PointUsageStatus;
+import com.impacus.maketplace.dto.point.greenLabelPoint.GreenLabelPointDTO;
+import com.impacus.maketplace.entity.point.greenLablePoint.QGreenLabelPoint;
 import com.impacus.maketplace.entity.point.greenLablePoint.QGreenLabelPointAllocation;
 import com.impacus.maketplace.repository.point.greenLabelPoint.mapping.NotUsedGreenLabelPointAllocationDTO;
 import com.querydsl.core.BooleanBuilder;
@@ -19,26 +21,27 @@ public class GreenLabelPointAllocationCustomRepositoryImpl implements GreenLabel
     private final JPAQueryFactory queryFactory;
     private final AuditorAware<String> auditorProvider;
 
-    private final QGreenLabelPointAllocation greenLabelPointAllocation = QGreenLabelPointAllocation.greenLabelPointAllocation;
+    private final QGreenLabelPoint greenLabelPoint = QGreenLabelPoint.greenLabelPoint1;
+    private final QGreenLabelPointAllocation allocation = QGreenLabelPointAllocation.greenLabelPointAllocation;
 
     @Override
     public List<NotUsedGreenLabelPointAllocationDTO> findNotUsedGreenLabelPointByUserId(Long userId) {
         BooleanBuilder builder = new BooleanBuilder();
-        builder.and(greenLabelPointAllocation.userId.eq(userId))
-                .and(greenLabelPointAllocation.pointStatus.in(List.of(PointUsageStatus.IN_USE, PointUsageStatus.UNUSED)));
+        builder.and(allocation.userId.eq(userId))
+                .and(allocation.pointStatus.in(List.of(PointUsageStatus.IN_USE, PointUsageStatus.UNUSED)));
 
         return queryFactory.select(
                         Projections.fields(
                                 NotUsedGreenLabelPointAllocationDTO.class,
-                                greenLabelPointAllocation.id,
-                                greenLabelPointAllocation.remainPoint,
-                                greenLabelPointAllocation.expiredAt,
-                                greenLabelPointAllocation.pointStatus
+                                allocation.id,
+                                allocation.remainPoint,
+                                allocation.expiredAt,
+                                allocation.pointStatus
                         )
                 )
-                .from(greenLabelPointAllocation)
+                .from(allocation)
                 .where(builder)
-                .orderBy(greenLabelPointAllocation.expiredAt.asc())
+                .orderBy(allocation.expiredAt.asc())
                 .fetch();
     }
 
@@ -47,15 +50,38 @@ public class GreenLabelPointAllocationCustomRepositoryImpl implements GreenLabel
         String currentAuditor = auditorProvider.getCurrentAuditor().orElse(null);
 
         queryFactory
-                .update(greenLabelPointAllocation)
-                .set(greenLabelPointAllocation.pointStatus, pointUsageStatus)
-                .set(greenLabelPointAllocation.remainPoint, remainPoint)
-                .set(greenLabelPointAllocation.expiredAt, LocalDateTime.now())
+                .update(allocation)
+                .set(allocation.pointStatus, pointUsageStatus)
+                .set(allocation.remainPoint, remainPoint)
+                .set(allocation.expiredAt, LocalDateTime.now())
 
-                .set(greenLabelPointAllocation.modifyAt, LocalDateTime.now())
-                .set(greenLabelPointAllocation.modifyId, currentAuditor)
-                .where(greenLabelPointAllocation.id.eq(id))
+                .set(allocation.modifyAt, LocalDateTime.now())
+                .set(allocation.modifyId, currentAuditor)
+                .where(allocation.id.eq(id))
                 .execute();
+    }
+
+    @Override
+    public GreenLabelPointDTO findPointInformationByUserId(Long userId) {
+        // 1. 그린 라벨 포인트 조회
+        Long point = queryFactory
+                .select(greenLabelPoint.greenLabelPoint)
+                .from(greenLabelPoint)
+                .where(greenLabelPoint.userId.eq(userId))
+                .fetchFirst();
+
+        // 2. 소멸 예정인 그린 라벨 포인트 조회
+        BooleanBuilder allocationBuilder = new BooleanBuilder();
+        allocationBuilder.and(allocation.userId.eq(userId))
+                .and(allocation.pointStatus.in(List.of(PointUsageStatus.UNUSED, PointUsageStatus.IN_USE)))
+                .and(allocation.expiredAt.between(LocalDateTime.now(), LocalDateTime.now().plusMonths(1)));
+        Long pointsExpiringIn30Days = queryFactory
+                .select(allocation.remainPoint.sum())
+                .from(allocation)
+                .where(allocationBuilder)
+                .fetchOne();
+
+        return GreenLabelPointDTO.toDTO(point, pointsExpiringIn30Days);
     }
 
 }
