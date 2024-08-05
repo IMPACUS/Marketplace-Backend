e2ca528253d26754f33f7e3d5c6de1cedaacfc92
chea-young <lchy0413@gmail.com>
2024-08-05 23:47:36 +0900

    [Feature] Add test code about GreenLabelPointAllocationService [Issue no] #140

--- src/main/java/com/impacus/maketplace/repository/point/greenLabelPoint/mapping/NotUsedGreenLabelPointAllocationDTO.java
+++ src/main/java/com/impacus/maketplace/repository/point/greenLabelPoint/mapping/NotUsedGreenLabelPointAllocationDTO.java
@@ -1,11 +1,13 @@
 package com.impacus.maketplace.repository.point.greenLabelPoint.mapping;
 
 import com.impacus.maketplace.common.enumType.point.PointUsageStatus;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@AllArgsConstructor
 public class NotUsedGreenLabelPointAllocationDTO {
     private Long id;
     private Long remainPoint;
