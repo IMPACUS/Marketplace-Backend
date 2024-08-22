321f7174813d558418dbc489c51c3d6c16e3b0f1
chea-young <lchy0413@gmail.com>
2024-08-23 01:37:34 +0900

    [Feature] Add DELETE /api/v1/develop/user [Issue no] #183

--- src/main/java/com/impacus/maketplace/repository/user/UserRepository.java
+++ src/main/java/com/impacus/maketplace/repository/user/UserRepository.java
@@ -2,7 +2,7 @@ package com.impacus.maketplace.repository.user;
 
 import com.impacus.maketplace.common.enumType.user.UserType;
 import com.impacus.maketplace.entity.user.User;
-import com.impacus.maketplace.repository.user.querydsl.ReadUserCustomRepository;
+import com.impacus.maketplace.repository.user.querydsl.UserCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>, ReadUserCustomRepository {
+public interface UserRepository extends JpaRepository<User, Long>, UserCustomRepository {
 
     List<User> findByEmailLike(String emailWithPrefix);
 
