ac8ffaccdfbc404c5c62f2138658e01899ec57d0
facewise <dydgus.k@gmail.com>
2024-09-24 11:18:27 +0900

    [feat] 주문 인터페이스 추가

---
+++ src/main/java/com/impacus/maketplace/service/api/PaymentEventInterface.java
@@ -0,0 +1,16 @@
+package com.impacus.maketplace.service.api;
+
+/**
+ * 주문 관련 인터페이스
+ */
+public interface PaymentEventInterface {
+
+    /**
+     * 주문 식별자(String)으로 PK 조회
+     *
+     * @param orderId 주문 식별자 (주문번호)
+     * @return 엔티티 PK
+     */
+    Long findIdByOrderId(String orderId);
+
+}
