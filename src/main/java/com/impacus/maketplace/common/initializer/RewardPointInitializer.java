bf5b933bd2702ae43838bc480ba7c03ef7a95484
chea-young <lchy0413@gmail.com>
2024-10-01 14:15:39 +0900

    [Refactor] Modify default point reward type storage logic [Issue no]  #209

--- src/main/java/com/impacus/maketplace/common/initializer/RewardPointInitializer.java
+++ src/main/java/com/impacus/maketplace/common/initializer/RewardPointInitializer.java
@@ -8,9 +8,7 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Component
 public class RewardPointInitializer {
@@ -24,21 +22,18 @@ public class RewardPointInitializer {
     @Transactional
     public void generateDummyRewardPointData() {
         List<RewardPointType> types = new ArrayList<>(Arrays.stream(RewardPointType.values()).toList());
-        List<RewardPointType> savedTypes = rewardPointRepository.findRewardPointType();
+        Set<RewardPointType> savedTypes = new HashSet<>(rewardPointRepository.findRewardPointType());
 
         // 1. 생성할 데이터가 존재하는지 확인
         types.removeAll(savedTypes);
         if (types.isEmpty()) {
-            return;
+            return; // 저장할 타입이 없음
         }
 
         // 2. RewardPointType 중 저장되지 않은 type 생성
-        List<RewardPoint> rewardPoints = new ArrayList<>();
-        for (RewardPointType type : types) {
-            if (!savedTypes.contains(type)) {
-                rewardPoints.add(RewardPoint.from(type));
-            }
-        }
+        List<RewardPoint> rewardPoints = types.stream()
+                .map(RewardPoint::from)
+                .toList();
 
         // 저장
         rewardPointRepository.saveAll(rewardPoints);
