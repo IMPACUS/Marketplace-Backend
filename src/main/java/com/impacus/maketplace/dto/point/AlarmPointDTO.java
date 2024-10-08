4c79cfdce2a4cd34383f9e50cf122ba4be25de2e
chea-young <lchy0413@gmail.com>
2024-10-09 07:48:08 +0900

    [Feature] Add phoneNumber in AlarmPointDTO [Issue no]

--- src/main/java/com/impacus/maketplace/dto/point/AlarmPointDTO.java
+++ src/main/java/com/impacus/maketplace/dto/point/AlarmPointDTO.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class AlarmPointDTO {
     private String userName; // 사용자 이름
+    private String phoneNumber; // 사용자 핸드폰 번호
     private Long remainPoint; // 소멸될 남은 포인트
     private LocalDateTime expiredAt; // 포인트 소멸 날짜
 }
