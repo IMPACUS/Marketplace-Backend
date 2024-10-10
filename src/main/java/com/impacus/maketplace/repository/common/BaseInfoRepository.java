17be3055b8b577e0662c230771c131edce2a3fee
chea-young <lchy0413@gmail.com>
2024-10-10 23:14:11 +0900

    [Feature] Add findRewardBaseInfo, updateRewardBaseInfo [Issue no] #219

--- src/main/java/com/impacus/maketplace/repository/common/BaseInfoRepository.java
+++ src/main/java/com/impacus/maketplace/repository/common/BaseInfoRepository.java
@@ -2,6 +2,7 @@ package com.impacus.maketplace.repository.common;
 
 import com.impacus.maketplace.common.enumType.common.InfoType;
 import com.impacus.maketplace.entity.common.BaseInfo;
+import com.impacus.maketplace.repository.common.querydsl.BaseInfoCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface BaseInfoRepository extends JpaRepository<BaseInfo, Long> {
+public interface BaseInfoRepository extends JpaRepository<BaseInfo, Long>, BaseInfoCustomRepository {
 
     @Query("select bi.infoType FROM BaseInfo bi")
     List<InfoType> findInfoType();
