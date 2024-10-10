0ac051638910c55d48822f9a525544dffec4d30a
chea-young <lchy0413@gmail.com>
2024-10-10 21:34:44 +0900

    [Feature] Add generateDummyBaseInfo to save default value [Issue no] #219

--- src/main/java/com/impacus/maketplace/repository/common/BaseInfoRepository.java
+++ src/main/java/com/impacus/maketplace/repository/common/BaseInfoRepository.java
@@ -1,9 +1,16 @@
 package com.impacus.maketplace.repository.common;
 
+import com.impacus.maketplace.common.enumType.common.InfoType;
 import com.impacus.maketplace.entity.common.BaseInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BaseInfoRepository extends JpaRepository<BaseInfo, Long> {
+
+    @Query("select bi.infoType FROM BaseInfo bi")
+    List<InfoType> findInfoType();
 }
