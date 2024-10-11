0d4eda8669178db0fce9fcfdf1a24ea0d73f9304
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-10-11 20:19:13 +0900

    [Feature] Implement getAlarmCoupon api service [Issue no] #221

--- src/main/java/com/impacus/maketplace/service/coupon/CouponApiServiceImpl.java
+++ src/main/java/com/impacus/maketplace/service/coupon/CouponApiServiceImpl.java
@@ -1,5 +1,6 @@
 package com.impacus.maketplace.service.coupon;
 
+import com.impacus.maketplace.dto.coupon.api.AlarmCouponDTO;
 import com.impacus.maketplace.dto.coupon.api.CouponNameDTO;
 import com.impacus.maketplace.repository.coupon.querydsl.CouponApiRepository;
 import com.impacus.maketplace.service.api.CouponApiService;
@@ -8,7 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -30,5 +34,17 @@ public class CouponApiServiceImpl implements CouponApiService {
     @Transactional
     public void issueCouponUser(Long userId, Long couponId) {
         couponIssuanceService.issueCouponTargetUserByAdmin(couponId, userId);
+    }
+
+    public Map<Long, List<AlarmCouponDTO>> getAlarmCoupon() {
+        List<AlarmCouponDTO> alarmCoupons = couponApiRepository.getAlarmCoupons();
+
+        Map<Long, List<AlarmCouponDTO>> alarmCouponsMap = new HashMap<>();
+
+        alarmCoupons.forEach(alarmCouponDTO ->
+                alarmCouponsMap.computeIfAbsent(alarmCouponDTO.getUserId(), k -> new ArrayList<>())
+        );
+
+        return alarmCouponsMap;
     }
 }
