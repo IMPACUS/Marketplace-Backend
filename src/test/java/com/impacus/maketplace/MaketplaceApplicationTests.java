1c8eb7a875223c8fc3051517d44a83beb7a12a10
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-10-18 14:35:40 +0900

    [Fix] Solving problem that occurred in the test phase of github actions 1. FirebaseApp Bean creation conflict [Issue no] #233

--- src/test/java/com/impacus/maketplace/MaketplaceApplicationTests.java
+++ src/test/java/com/impacus/maketplace/MaketplaceApplicationTests.java
@@ -1,14 +1,11 @@
 package com.impacus.maketplace;
 
-import com.impacus.maketplace.config.TestFirebaseConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@Import(TestFirebaseConfig.class)
 class MaketplaceApplicationTests {
 
 	@Test
