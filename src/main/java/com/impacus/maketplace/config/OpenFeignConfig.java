24d1e237c0f96ff6cb2aa90de5b6c3d43a4d7dbf
chea-young <lchy0413@gmail.com>
2024-10-22 20:27:38 +0900

    [Feature] Add openfeign setting [Issue no] #236

---
+++ src/main/java/com/impacus/maketplace/config/OpenFeignConfig.java
@@ -0,0 +1,21 @@
+package com.impacus.maketplace.config;
+
+import feign.Retryer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableFeignClients(basePackages = {
+
+})
+public class OpenFeignConfig {
+
+    @Bean
+    public Retryer feignRetryer() {
+        // 1000ms 후 처음 재시도를 하고, 이후 10초 간격으로 최대 10번까지 재시도하도록 설정
+        return new Retryer.Default(1000, TimeUnit.SECONDS.toMillis(10), 10);
+    }
+}
