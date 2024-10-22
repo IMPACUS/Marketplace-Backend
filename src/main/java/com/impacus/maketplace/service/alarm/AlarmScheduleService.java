7f9e8a494b6092cdadd199835d654d9060df4de4
DongA Shin <82761686+DoItDoA@users.noreply.github.com>
2024-10-22 18:43:19 +0900

    [Feature] 알림 임시 작업

--- src/main/java/com/impacus/maketplace/service/alarm/AlarmScheduleService.java
+++ src/main/java/com/impacus/maketplace/service/alarm/AlarmScheduleService.java
@@ -26,9 +26,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 @Slf4j
