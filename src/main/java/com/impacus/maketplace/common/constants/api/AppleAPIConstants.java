a641f754e5c25fd39fbef94f54f6552d96225c79
chea-young <lchy0413@gmail.com>
2024-10-29 22:29:02 +0900

    [Feature] Add logic to get apple token and save [Issue no] #236

---
+++ src/main/java/com/impacus/maketplace/common/constants/api/AppleAPIConstants.java
@@ -0,0 +1,7 @@
+package com.impacus.maketplace.common.constants.api;
+
+public final class AppleAPIConstants {
+    public static final String COMMON_URL = "https://appleid.apple.com";
+    public static final String AUTH_URL = COMMON_URL + "/auth";
+    public static final String VALIDATE_CODE = "/token";
+}
---
+++ src/main/java/com/impacus/maketplace/service/oauth/apple/AppleOAuthAPIService.java
@@ -0,0 +1,23 @@
+package com.impacus.maketplace.service.oauth.apple;
+
+import com.impacus.maketplace.common.constants.api.AppleAPIConstants;
+import com.impacus.maketplace.dto.oauth.apple.AppleTokenResponse;
+import com.impacus.maketplace.service.oauth.OAuthAPIServiceFallback;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "appleApiClient",
+        url = AppleAPIConstants.AUTH_URL,
+        fallback = OAuthAPIServiceFallback.class
+)
+public interface AppleOAuthAPIService {
+
+    @PostMapping(value = AppleAPIConstants.VALIDATE_CODE)
+    AppleTokenResponse getToken(
+            @RequestParam("client_id") String clientId,
+            @RequestParam("client_secret") String clientSecret,
+            @RequestParam("code") String code,
+            @RequestParam("grant_type") String grantType
+    );
+}
