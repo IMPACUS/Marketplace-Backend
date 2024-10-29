dff10c8adfa5d9afbde6d592f24f829d0235646d
chea-young <lchy0413@gmail.com>
2024-10-29 22:29:02 +0900

    [Feature] Add logic to get apple token and save [Issue no] #236

--- src/main/java/com/impacus/maketplace/service/oauth/OAuthAPIServiceFallback.java
+++ src/main/java/com/impacus/maketplace/service/oauth/OAuthAPIServiceFallback.java
@@ -2,16 +2,23 @@ package com.impacus.maketplace.service.oauth;
 
 import com.impacus.maketplace.common.enumType.error.CommonErrorType;
 import com.impacus.maketplace.common.exception.CustomException;
+import com.impacus.maketplace.dto.oauth.apple.AppleTokenResponse;
 import com.impacus.maketplace.dto.oauth.kakao.KakaoTokenResponse;
 import com.impacus.maketplace.dto.oauth.kakao.userProfile.KakaoUserProfileResponse;
 import com.impacus.maketplace.dto.oauth.naver.NaverTokenResponse;
 import com.impacus.maketplace.dto.oauth.naver.userProfile.NaverUserResponse;
+import com.impacus.maketplace.service.oauth.apple.AppleOAuthAPIService;
 import com.impacus.maketplace.service.oauth.kakao.KakaoCommonAPIService;
 import com.impacus.maketplace.service.oauth.kakao.KakaoOAuthAPIService;
 import com.impacus.maketplace.service.oauth.naver.NaverCommonAPIService;
 import com.impacus.maketplace.service.oauth.naver.NaverOAuthAPIService;
 
-public class OAuthAPIServiceFallback implements KakaoOAuthAPIService, KakaoCommonAPIService, NaverOAuthAPIService, NaverCommonAPIService {
+public class OAuthAPIServiceFallback implements
+        KakaoOAuthAPIService,
+        KakaoCommonAPIService,
+        NaverOAuthAPIService,
+        NaverCommonAPIService,
+        AppleOAuthAPIService {
 
     @Override
     public KakaoTokenResponse getTokenInfo(
@@ -36,6 +43,11 @@ public class OAuthAPIServiceFallback implements KakaoOAuthAPIService, KakaoCommo
 
     @Override
     public NaverUserResponse getUser(String authorization) {
+        throw new CustomException(CommonErrorType.OPEN_API_REQUEST_FAIL);
+    }
+
+    @Override
+    public AppleTokenResponse getToken(String clientId, String clientSecret, String code, String grantType) {
         throw new CustomException(CommonErrorType.OPEN_API_REQUEST_FAIL);
     }
 }
