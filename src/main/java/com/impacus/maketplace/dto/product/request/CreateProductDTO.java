d658a3e97e341df953b3edb09115f9847fd3d731
chea-young <lchy0413@gmail.com>
2024-11-05 00:10:35 +0900

    [Feature] Add CreateProductDTO constructor [Issue no]

--- src/main/java/com/impacus/maketplace/dto/product/request/CreateProductDTO.java
+++ src/main/java/com/impacus/maketplace/dto/product/request/CreateProductDTO.java
@@ -1,5 +1,7 @@
 package com.impacus.maketplace.dto.product.request;
 
+import com.impacus.maketplace.common.enumType.DeliveryCompany;
+import com.impacus.maketplace.common.enumType.product.*;
 import com.impacus.maketplace.entity.product.Product;
 import com.impacus.maketplace.entity.temporaryProduct.TemporaryProduct;
 import jakarta.validation.constraints.NotNull;
@@ -27,6 +29,48 @@ public class CreateProductDTO extends ProductDTO {
 
     public TemporaryProduct toTemporaryEntity() {
         return new TemporaryProduct(this);
+    }
+
+    public CreateProductDTO(
+            Long sellerId,
+            boolean doesUseTemporaryProduct,
+            String name,
+            DeliveryType deliveryType,
+            Boolean isCustomProduct,
+            Long categoryId,
+            DeliveryRefundType deliveryFeeType,
+            DeliveryRefundType refundFeeType,
+            Integer deliveryFee,
+            Integer refundFee,
+            Integer specialDeliveryFee,
+            Integer specialRefundFee,
+            DeliveryCompany deliveryCompany,
+            BundleDeliveryOption bundleDeliveryOption,
+            Long bundleDeliveryGroupId,
+            List<String> productImages,
+            Integer marketPrice,
+            Integer appSalesPrice,
+            Integer discountPrice,
+            Integer weight,
+            ProductStatus productStatus,
+            String description,
+            ProductType type,
+            Integer salesChargePercent,
+            CreateProductDetailInfoDTO productDetail,
+            List<CreateProductOptionDTO> productOptions,
+            CreateProductDeliveryTimeDTO deliveryTime,
+            CreateClaimInfoDTO claim
+    ) {
+        super(
+                name, deliveryType, isCustomProduct, categoryId, deliveryFeeType, refundFeeType,
+                deliveryFee, refundFee, specialDeliveryFee, specialRefundFee, deliveryCompany,
+                bundleDeliveryOption, bundleDeliveryGroupId, marketPrice, appSalesPrice, discountPrice,
+                weight, productStatus, description, type, salesChargePercent, productDetail,
+                productOptions, deliveryTime, claim
+        );
+        this.sellerId = sellerId;
+        this.doesUseTemporaryProduct = doesUseTemporaryProduct;
+        this.productImages = productImages;
     }
 
 }
