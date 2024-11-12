bd4ab0b59ea9dfd3d1a9aada6d65f946718168f8
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-11-12 19:00:25 +0900

    [Feature] Modify API GET /api/v1/coupon/admin/{coupon-id}/details 1. add eventType field to response [Issue no] #290

--- src/main/java/com/impacus/maketplace/dto/coupon/request/CouponEventTypeDTO.java
+++ src/main/java/com/impacus/maketplace/dto/coupon/request/CouponEventTypeDTO.java
@@ -1,6 +1,5 @@
 package com.impacus.maketplace.dto.coupon.request;
 
-import com.impacus.maketplace.common.enumType.coupon.EventType;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
