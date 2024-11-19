2877cc42c19f77654124b385ace9de70a5744c79
chea-young <lchy0413@gmail.com>
2024-11-19 23:40:47 +0900

    [Feature] Change the RedisHash value of CertificationRequestNumber [Issue no] #260

--- src/main/java/com/impacus/maketplace/redis/entity/CertificationRequestNumber.java
+++ src/main/java/com/impacus/maketplace/redis/entity/CertificationRequestNumber.java
@@ -8,7 +8,7 @@ import org.springframework.data.redis.core.index.Indexed;
 
 @Getter
 @NoArgsConstructor
-@RedisHash(value = "fileStatus", timeToLive = 86400L)
+@RedisHash(value = "certificationRequestNumber", timeToLive = 86400L)
 public class CertificationRequestNumber {
     @Id
     private String id;
