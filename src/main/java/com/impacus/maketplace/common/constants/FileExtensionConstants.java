1518ace35943facfc629bd67e9343d94cf7e8ff1
chea-young <lchy0413@gmail.com>
2024-11-20 21:33:00 +0900

    [Feature] Add exception handling about product images extension [Issue no]

---
+++ src/main/java/com/impacus/maketplace/common/constants/FileExtensionConstants.java
@@ -0,0 +1,7 @@
+package com.impacus.maketplace.common.constants;
+
+import java.util.List;
+
+public final class FileExtensionConstants {
+    public static final List<String> PRODUCT_IMAGE_EXTENSION = List.of(new String[]{"png", "jpg", "jpeg"});
+}
