586ad67bad8c33877f0caa857e80c6a7f6085da6
cheayoung.lee <cheayoung.lee@lge.com>
2024-11-26 13:45:53 +0900

    [Feature] Add certification-server.html [Issue no] #260

--- src/main/java/com/impacus/maketplace/config/WebConfiguration.java
+++ src/main/java/com/impacus/maketplace/config/WebConfiguration.java
@@ -2,6 +2,7 @@ package com.impacus.maketplace.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -19,5 +20,11 @@ public class WebConfiguration implements WebMvcConfigurer {
                         "https://dev.implace.kr"
                 )
                 .allowedMethods("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/html");
     }
 }
