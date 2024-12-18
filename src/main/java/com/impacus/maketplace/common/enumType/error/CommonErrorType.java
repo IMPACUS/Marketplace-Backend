d0b9f339f79a282f274bcd831c33c2121cd7b5aa
chea-young <lchy0413@gmail.com>
2024-12-18 22:49:52 +0900

    [Fix] Resolve merge error [Issue no]

--- src/main/java/com/impacus/maketplace/common/enumType/error/CommonErrorType.java
+++ src/main/java/com/impacus/maketplace/common/enumType/error/CommonErrorType.java
@@ -24,9 +24,6 @@ public enum CommonErrorType implements ErrorType {
     FAIL_TO_SEND_EMAIL("32_FAIL_TO_SEND_EMAIL", "이메일 전송에 실패하였습니다."),
     INVALID_ALARM("043_INVALID_ALARM", "유효하지 않는 알림 타입입니다."),
     INVALID_ID("044_INVALID_ID", "유효하지 않은 ID 입니다."),
-
-    DUPLICATED_SUB_CATEGORY("050_DUPLICATED_SUB_CATEGORY", "중복된 2차 카테고리 명이 존재합나디."),
-
     INVALID_END_DATE("060_INVALID_END_DATE", "종료날짜는 시작날짜보다 이전일 수 없습니다."),
 
     // 401
