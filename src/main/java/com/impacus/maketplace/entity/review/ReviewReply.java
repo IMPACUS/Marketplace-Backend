15efeb5d79fb9bae2479d2ed98845e2af9005ed2
cheayoung.lee <cheayoung.lee@lge.com>
2024-12-30 10:36:12 +0900

    [Feature] Add ReviewRelay [Issue no] #104

---
+++ src/main/java/com/impacus/maketplace/entity/review/ReviewReply.java
@@ -0,0 +1,21 @@
+package com.impacus.maketplace.entity.review;
+
+import com.impacus.maketplace.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "review_reply")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReviewReply extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_reply_id")
+    private Long id;
+
+    @Column(name = "review_id", nullable = false, unique = true)
+    private Long reviewId;
+
+    @Column(name = "contents", nullable = false, columnDefinition = "TEXT")
+    private String contents;
+}
