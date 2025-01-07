9a8a3fb2191e1460e345ee2ff0bab78fdf16362a
chea-young <lchy0413@gmail.com>
2025-01-07 20:56:57 +0900

    [Feature] Add validate about review - 등록된 리뷰인지 확인하는 유효성 검사 추 [Issue no] #104

--- src/main/java/com/impacus/maketplace/repository/review/ReviewRepository.java
+++ src/main/java/com/impacus/maketplace/repository/review/ReviewRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewCustomRepository {
     boolean existsByIdAndIsDeletedFalse(Long reviewId);
+
+    boolean existsByOrderIdAndProductOptionId(Long orderId, Long productOptionId);
 }
