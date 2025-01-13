e8bac250a00322e52a9a264197f0fdd5a7db8cd9
chea-young <lchy0413@gmail.com>
2025-01-13 22:25:23 +0900

    [Feature] Add oauth_provider_type [Issue no] #336

--- src/main/java/com/impacus/maketplace/entity/user/User.java
+++ src/main/java/com/impacus/maketplace/entity/user/User.java
@@ -3,6 +3,7 @@ package com.impacus.maketplace.entity.user;
 import com.impacus.maketplace.common.BaseEntity;
 import com.impacus.maketplace.common.constants.DaysConstants;
 import com.impacus.maketplace.common.converter.AES256ToStringConverter;
+import com.impacus.maketplace.common.enumType.user.OauthProviderType;
 import com.impacus.maketplace.common.enumType.user.UserType;
 import com.impacus.maketplace.common.utils.TimestampConverter;
 import com.impacus.maketplace.dto.user.PhoneNumberDTO;
@@ -28,6 +29,10 @@ public class User extends BaseEntity {
 
     @Column(nullable = false)
     private String email; // Format: OauthProviderKey_Email
+
+    @Column(nullable = false, columnDefinition = "NONE")
+    @Enumerated(EnumType.STRING)
+    private OauthProviderType oauthProviderType; // 소셜 로그인 제공자
 
     @Convert(converter = AES256ToStringConverter.class)
     private String password; // 비밀번호
