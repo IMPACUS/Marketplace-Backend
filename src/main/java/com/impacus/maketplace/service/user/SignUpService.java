523b5dc1282532c5249504d2e05e69fb648374ec
chea-young <lchy0413@gmail.com>
2025-02-04 20:03:08 +0900

    [Feature] Separate SignUpService - 본인인증 과정 함수를 SignUpService로 이동 [Issue no]

--- src/main/java/com/impacus/maketplace/service/user/SignUpService.java
+++ src/main/java/com/impacus/maketplace/service/user/SignUpService.java
@@ -5,9 +5,12 @@ import com.impacus.maketplace.common.enumType.user.OauthProviderType;
 import com.impacus.maketplace.common.enumType.user.UserType;
 import com.impacus.maketplace.common.exception.CustomException;
 import com.impacus.maketplace.common.utils.StringUtils;
+import com.impacus.maketplace.dto.auth.CertificationResult;
 import com.impacus.maketplace.dto.user.request.SignUpDTO;
 import com.impacus.maketplace.dto.user.response.UserDTO;
+import com.impacus.maketplace.entity.consumer.Consumer;
 import com.impacus.maketplace.entity.user.User;
+import com.impacus.maketplace.repository.consumer.ConsumerRepository;
 import com.impacus.maketplace.repository.user.UserRepository;
 import com.impacus.maketplace.service.UserService;
 import com.impacus.maketplace.service.alarm.user.AlarmUserService;
@@ -16,6 +19,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -28,6 +33,7 @@ public class SignUpService {
     private final PointService pointService;
     private final AlarmUserService alarmUserService;
     private final UserDeactivationService userDeactivationService;
+    private final ConsumerRepository consumerRepository;
 
     @Transactional
     public UserDTO addUser(SignUpDTO signUpRequest) {
@@ -91,6 +97,69 @@ public class SignUpService {
         }
 
         return true;
+    }
+
+    /**
+     * 사용자 개인 정보 저장
+     *
+     * @param userId              사용자 ID
+     * @param certificationResult 본인 인증 데이터
+     */
+    @Transactional
+    public void saveCertification(Long userId, CertificationResult certificationResult) {
+        // 1. 사용자 존재하는지 확인
+        if (!userRepository.existsByIdAndType(
+                userId,
+                List.of(UserType.ROLE_CERTIFIED_USER, UserType.ROLE_UNCERTIFIED_USER)
+        )) {
+            throw new CustomException(UserErrorType.NOT_EXISTED_USER);
+        }
+
+        // 2. 이미 존재하는 핸드폰 번호인지 확인
+        if (userRepository.existsConsumerByPhoneNumberAndUserId(
+                userId,
+                certificationResult.getMobileNo())
+        ) {
+            throw new CustomException(UserErrorType.DUPLICATED_PHONE_NUMBER);
+        }
+
+        // 2. 저장 혹은 업데이트
+        userRepository.saveOrUpdateCertification(userId, certificationResult);
+        saveOrUpdateConsumer(userId, certificationResult);
+    }
+
+    /**
+     * Consumer 정보 저장 혹은 업데이트
+     *
+     * @param userId              사용자 ID
+     * @param certificationResult 본인 인증 데이터
+     */
+    @Transactional
+    public void saveOrUpdateConsumer(Long userId, CertificationResult certificationResult) {
+        // 회원가입 가능한 CI 인지 확인(+ 탈퇴한 회원의 CI 인지 확인)
+        validateUserRejoinable(certificationResult.getCi());
+
+        if (consumerRepository.existsByUserId(userId)) { // 업데이트
+            consumerRepository.updateConsumer(userId, certificationResult.getCi(), LocalDateTime.now());
+        } else { // 생성
+            Consumer consumer = certificationResult.toEntity(userId);
+            consumerRepository.save(consumer);
+        }
+    }
+
+    private void validateUserRejoinable(String ci) {
+        User user = userRepository.findUserByCI(ci);
+        if (user == null) {
+            return;
+        }
+
+        if (user.isDeleted()) {
+            if (!user.isRejoinable()) {
+                throw new CustomException(UserErrorType.FAIL_TO_REJOIN_14);
+            }
+        } else {
+            throw new CustomException(UserErrorType.DUPLICATED_CI);
+        }
     }
 
 }
