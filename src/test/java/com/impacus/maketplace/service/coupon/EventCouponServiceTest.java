a99ebbf3eb8d1ed868c95b3397195b2d4b0de7a7
jeongbeomSeo <jeongbeom4693@gmail.com>
2025-02-18 14:50:50 +0900

    [Test] test CouponPrioritySelector [Issue no] #335

---
+++ src/test/java/com/impacus/maketplace/service/coupon/EventCouponServiceTest.java
@@ -0,0 +1,11 @@
+package com.impacus.maketplace.service.coupon;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Event Coupon Service 테스트")
+class EventCouponServiceTest {
+
+}
