8d9fe6622ff2c1da958f63d27d1de0d853b0d692
lchy0413 <lchy0413@gmail.com>
2025-03-15 15:13:46 +0900

    [Feature] Add statisticsData in StatisticsPeriod [Issue no] #348

--- src/main/java/com/impacus/maketplace/common/enumType/StatisticsPeriod.java
+++ src/main/java/com/impacus/maketplace/common/enumType/StatisticsPeriod.java
@@ -10,11 +10,11 @@ import java.util.List;
 @Getter
 @RequiredArgsConstructor
 public enum StatisticsPeriod {
-    YESTERDAY("어제", new int[] {0, 5, 9, 13, 17, 21, 24}),
-    LAST_7_DAYS("최근 7일간", new int[] {}),
-    LAST_30_DAYS("최근 한 달간", new int[] {}),
-    MONTHLY("월간", new int[] {}),
-    YEARLY("연간", new int[] {});
+    YESTERDAY("어제", new int[] {0, 5, 9, 13, 17, 21, 24}), // 시간
+    LAST_7_DAYS("최근 7일간", new int[] {1, 2,3,4,5,6,7}), // 일 전
+    LAST_30_DAYS("최근 한 달간", new int[] {}), // 주차
+    MONTHLY("월간", new int[] {1,2,3,4,5,6,7,8}), // 개월 전
+    YEARLY("연간", new int[] {1, 2, 3,4,5,6,7}); // 년 전
 
     private final String description;
     private final int[] statisticsData;
