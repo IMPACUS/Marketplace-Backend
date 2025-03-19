bfcbc9a04c041e097e056d6bd986670d4ab3de8f
lchy0413 <lchy0413@gmail.com>
2025-03-19 16:22:47 +0900

    [Feature] Add API path in StatisticController [Issue no] #348

--- src/main/java/com/impacus/maketplace/controller/StatisticController.java
+++ src/main/java/com/impacus/maketplace/controller/StatisticController.java
@@ -1,6 +1,8 @@
 package com.impacus.maketplace.controller;
 
+import com.impacus.maketplace.common.utils.ApiResponseEntity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/statistics")
 public class StatisticController {
+
+
+    /**
+     * 통계 그래프 API
+     * @return
+     */
+    @GetMapping("/activities/chart")
+    public ApiResponseEntity<?> getUserActivitiesInChart() {
+        return null;
+    }
+
+
+    /**
+     * 유저/판매자 활동 API
+     * @return
+     */
+    @GetMapping("/activities/table")
+    public ApiResponseEntity<?> getUserActivitiesInTable() {
+        return null;
+    }
 }
